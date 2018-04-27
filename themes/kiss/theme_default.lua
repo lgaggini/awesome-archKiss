8e9668cecbf27e12fde7e7c595e52fad7d9ce35c
Lorenzo Gaggini <lg@lgaggini.net>
2018-04-27 15:08:09 +0200

    fixes border width

--- themes/kiss/theme_default.lua
+++ themes/kiss/theme_default.lua
@@ -36,7 +36,7 @@ theme.fg_urgent     = "#ffffff"
 theme.fg_minimize   = "#ffffff"
 
 -- Main borders
-theme.border_width  = 1
+theme.border_width  = "1"
 theme.border_normal = "#000000"
 theme.border_focus  = "#535d6c"
 theme.border_marked = "#91231c"
