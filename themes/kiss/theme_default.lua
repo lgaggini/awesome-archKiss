2a807368f675a767ecadd12ad9ce16d90df4dfaa
Lorenzo Gaggini <lg@lgaggini.net>
2019-07-08 10:42:23 +0200

    more readable fg color

--- themes/kiss/theme_default.lua
+++ themes/kiss/theme_default.lua
@@ -30,7 +30,7 @@ theme.bg_urgent     = "#ff0000"
 theme.bg_minimize   = "#444444"
 theme.bg_systray    = theme.bg_normal
 
-theme.fg_normal     = "#aaaaaa"
+theme.fg_normal     = "#cccccc"
 theme.fg_focus      = "#ffffff"
 theme.fg_urgent     = "#ffffff"
 theme.fg_minimize   = "#ffffff"
